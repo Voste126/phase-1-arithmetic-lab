dc1207075faf48bb76317d6238251315bccfa9aa
voste126 <steveaustine126@gmail.com>
2023-06-12 17:21:14 +0300

    arithematics

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // multiply = 62
+
+let random = Math.floor(Math.random() * 100) + 1; // generates a random integer greater than 0
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // mod = 4
+
+let numberSet = [10, 7, 5, 14, 20];
+let max = Math.max(...numberSet); // max = 20
